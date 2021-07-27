d5c85e9853cd19ba8a54484cd85354273726ac51
Josh Taylor <josh@joshtaylordesign.com>
2021-07-27 09:18:32 -0700

    update

--- uppercasePhrase.js
+++ uppercasePhrase.js
@@ -1,14 +1,21 @@
-console.log("test");
 
 //given a phrase, capitalize every word and
 
 const test = "this is a test of the algorithm";
 
-const capitalize = (a) => {
-    const array = a.split(" ");
-    console.log(array[1][0]);
-    console.log(array[1][0].toUpperCase());
+const array = test.split(" ");
 
+
+
+const capitalize = (a) => {
+    // const array = a.split(" ");
+    // console.log(array[1][0]);
+    // console.log(array[1][0].toUpperCase());
+    // a[0] = a[0].toUpperCase();
+    console.log(a[0].toUpperCase())
+    a
+    console.log(a)
 }
 
-capitalize(test);
+const newArray = array.map(capitalize)
+
