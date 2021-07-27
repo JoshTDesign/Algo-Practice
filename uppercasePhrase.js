5d6bb74fad01f551cdc6af70784874f13311c7a7
Josh Taylor <josh@joshtaylordesign.com>
2021-07-27 09:05:39 -0700

    uppercase phrase practice algo

---
+++ uppercasePhrase.js
@@ -0,0 +1,14 @@
+console.log("test");
+
+//given a phrase, capitalize every word and
+
+const test = "this is a test of the algorithm";
+
+const capitalize = (a) => {
+    const array = a.split(" ");
+    console.log(array[1][0]);
+    console.log(array[1][0].toUpperCase());
+
+}
+
+capitalize(test);
